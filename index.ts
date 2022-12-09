de11326988607a9d2d9102fdf604b8a7065d3385
Ali Muradov <amuradov239@gmail.com>
2022-12-09 16:31:07 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,10 +4,24 @@ const User = {
   isActive: false,
 };
 
-const createUser = ({ name: string, isPaid: boolean }) => {};
+// const createUser = ({ name: string, isPaid: boolean }) => {};
 
-createUser({ name: "ali", isPaid: false });
+// createUser({ name: "ali", isPaid: false });
 
 const createCourse = (): { name: string; price: number } => {
   return { name: "strings", price: 3123 };
+};
+
+type User = {
+  readonly _id: string;
+  name: string;
+  email: string;
+  isActive: boolean;
+};
+
+let myUser: User = {
+  _id: "123",
+  name: "hhh",
+  email: "asda",
+  isActive: false,
 };

66122aa88b0d0126058483c7d56a0f144de47d58
Ali Muradov <amuradov239@gmail.com>
2022-12-12 10:04:12 +0200

    type predicates and instance of examples

--- dist/src/typeNarrowing.js
+++ dist/src/typeNarrowing.js
@@ -17,3 +17,25 @@ function isAdmin(account) {
         return account.isAdmin;
     }
 }
+//TODO: INSTANCEOF KEYWORD AND ADDITIONAL LOGIC FOR TYPE PREDICATES
+function logValue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    }
+    else {
+        console.log(x.toUpperCase());
+    }
+}
+function isFish(pet) {
+    return pet.swim !== undefined;
+}
+function getFood(pet) {
+    if (isFish(pet)) {
+        pet;
+        return "fish food";
+    }
+    else {
+        pet;
+        return "bird fppd";
+    }
+}
--- src/typeNarrowing.ts
+++ src/typeNarrowing.ts
@@ -32,3 +32,30 @@ function isAdmin(account: User | Admin) {
     return account.isAdmin;
   }
 }
+
+//TODO: INSTANCEOF KEYWORD AND ADDITIONAL LOGIC FOR TYPE PREDICATES
+
+function logValue(x: Date | string) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toUpperCase());
+  }
+}
+
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+  if (isFish(pet)) {
+    pet;
+    return "fish food";
+  } else {
+    pet;
+    return "bird fppd";
+  }
+}
